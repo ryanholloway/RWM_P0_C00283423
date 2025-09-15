9570f8cb2512d818e08d779508740b5583729953
Ryan Holloway <C00283423@setu.ie>
2025-09-15 12:09:48 +0100

    failing test

--- src/lib/filters/removeOddNumbers.test.ts
+++ src/lib/filters/removeOddNumbers.test.ts
@@ -4,7 +4,7 @@ import { removeOddNumbers } from "./removeOddNumbers";
 describe("Remove Odd Numbers Filter", () => {
   it("Removes all odd numbers", () => {
     const input = [1, 2, 3, 4, 5, 6, 7, 9, 999];
-    const expected = [2, 4, 6];
+    const expected = [2, 4, , 6, 7];
     expect(removeOddNumbers(input)).toEqual(expected);
   });
 });
