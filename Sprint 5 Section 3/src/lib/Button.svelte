df915a18b863a7474f81dd79f1cfb7a7be173188
Ryan Holloway <C00283423@setu.ie>
2025-09-29 10:20:47 +0100

    Button base

---
+++ Sprint 5 Section 3/src/lib/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let label: string = 'Click me';
+</script>
+
+<button class="btn" on:click={() => console.log('clicked')}>
+	{label}
+</button>
+
+<style>
+	.btn {
+		padding: 0.5rem 1rem;
+		background: #eeeeee;
+		color: #222222;
+		border-radius: 0.375rem;
+	}
+</style>
